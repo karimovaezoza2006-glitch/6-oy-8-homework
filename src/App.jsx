269f1134c6ae059754a316caceb36e7bb3e348ac
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-12-12 10:37:12 +0500

    count

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { createContext } from 'react'
 import Components1 from './components/Components1';
 import { useState } from 'react';
 
+
+ export const Context= createContext()
 const App = () => {
+ 
   const [count, setCount] =useState(0);
   return (
-    <div className='border p-4 m-4'>
+    <Context.Provider value={{ count, setCount}}>
+      <div className='border p-4 m-4'>
       <h2> App {count }</h2>
       <Components1 />
     </div>
+    </Context.Provider>
   )
 }
 
--- src/components/Components3.jsx
+++ src/components/Components3.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-
+import { useContext } from 'react';
+import { Context } from '../App';
 const Components3 = () => {
+  const {setCount} = useContext(Context);
   return (
     <div className='border p-4'>
       <h2>Components3</h2>
-      <button>+</button>
-      <button>-</button>
+      <button onClick={() => setCount ((prev) => prev +1 )}>+</button>
+      <button onClick={() => setCount ((prev) => prev -1 )}>-</button>
       </div>
   )
 }
